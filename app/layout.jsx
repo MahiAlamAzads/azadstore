f9dd5eacb3501d9b8bee477240620c41ee8dd899
mahialamazads <mahialamazads@gmail.com>
2025-09-19 21:14:49 +0600

    initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,23 +2,34 @@ import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,   
+} from "@clerk/nextjs"; //added
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
-    title: "GoCart. - Shop smarter",
-    description: "GoCart. - Shop smarter",
+  title: "GoCart. - Shop smarter",
+  description: "GoCart. - Shop smarter",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className={`${outfit.className} antialiased`}>
-                <StoreProvider>
-                    <Toaster />
-                    {children}
-                </StoreProvider>
-            </body>
-        </html>
-    );
+  return (
+    <ClerkProvider> 
+        {/* ADDED */}
+      <html lang="en">
+        <body className={`${outfit.className} antialiased`}>
+          <StoreProvider>
+            <Toaster />
+            {children}
+          </StoreProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
